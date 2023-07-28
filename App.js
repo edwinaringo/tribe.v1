f4b6c77547d4d12623393b631322d0a5d8f2bade
edwinaringo <tedaringo628@gmail.com>
2023-07-28 12:54:06 +0300

    Removed the create new tribe files

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import { View, Text } from "react-native";
 import * as React from 'react'
 import HomeScreen from "./screens/HomeScreen";
-import CreateTribeScreen from "./screens/CreateTribeScreen";
+// import CreateTribeScreen from "./screens/CreateTribeScreen";
 
 export default function App() {
-  return <CreateTribeScreen/>
+  return <HomeScreen/>
   
 }
