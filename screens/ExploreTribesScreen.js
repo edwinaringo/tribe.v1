e8f2946b50a4746b557df402fdce132e62cea291
edwinaringo <tedaringo628@gmail.com>
2023-09-18 18:32:55 +0300

    fixed the displaying of tribe bug

--- screens/ExploreTribesScreen.js
+++ screens/ExploreTribesScreen.js
@@ -1,14 +1,42 @@
 import { View, Text, StyleSheet, ScrollView, Keyboard, Animated } from 'react-native'
-import React,{useCallback, useEffect} from 'react'
+import React,{useCallback, useEffect, useState} from 'react'
 import SearchBar from '../components/explore/SearchBar'
 import ExploreBottomTabs, { bottomTabIcons } from '../components/home/BottomTabs'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import CategorySection from '../components/explore/CategorySection'
 import ExploreTribesPosts from '../components/exploreTribes/ExploreTribesPosts'
 import { TRIBES } from '../data/tribes'
+import { getDocs, collection, getFirestore, collectionGroup } from 'firebase/firestore';
+import { FIRESTORE_DB } from '../firebase'
 
 
 const ExploreTribesScreen = ({navigation}) => {
+
+  const db = getFirestore(FIRESTORE_DB)
+  
+  const [tribes, setTribes] = useState([])
+
+  useEffect(() => {
+    const fetchTribes = async () => {
+      try {
+        const collectionRef = collectionGroup(db, 'tribes')
+        console.log("Starting process")
+
+        const querySnapshot = await getDocs(collectionRef)        
+        const tribeData = querySnapshot.docs.map((doc) => doc.data())
+        console.log("Query Snapshot:", querySnapshot.docs.map((doc) => doc.data()))
+
+        setTribes(tribeData);
+        console.log("Here are your tribes:", tribeData)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    
+    }
+    fetchTribes();
+
+  
+  }, []);
 
     
   return (
@@ -16,18 +44,19 @@ const ExploreTribesScreen = ({navigation}) => {
         <ScrollView>
              <SearchBar />
              <CategorySection navigation={navigation}/>
-             {TRIBES.map((tribe, index)=>(
-                <ExploreTribesPosts tribe = {tribe} key={index} navigation={navigation}/>
-             )
-             )}
+             {tribes.length === 0 ? (
+                <Text>No tribes found.</Text>
+              ) : (
+                tribes.map((tribe, index) => (
+                  <ExploreTribesPosts tribe={tribe} key={index} navigation={navigation} />
+                ))
+              )}
              
         </ScrollView>       
         <ExploreBottomTabs icons={bottomTabIcons} />
     </SafeAreaView>
     )
 }
-
-
 
 const styles= StyleSheet.create({
     container: {
